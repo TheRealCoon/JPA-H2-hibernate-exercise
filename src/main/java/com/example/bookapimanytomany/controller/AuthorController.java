efc5689fa803dea89f5efbd69de128886368a6fa
T.Sanyi <s.truczkai@gmail.com>
2023-03-27 10:50:27 +0200

    add operation to swagger

--- src/main/java/com/example/bookapimanytomany/controller/AuthorController.java
+++ src/main/java/com/example/bookapimanytomany/controller/AuthorController.java
@@ -3,9 +3,10 @@ package com.example.bookapimanytomany.controller;
 import com.example.bookapimanytomany.model.Author;
 import com.example.bookapimanytomany.model.AuthorDTO;
 import com.example.bookapimanytomany.model.Book;
-import com.example.bookapimanytomany.model.BookDTO;
 import com.example.bookapimanytomany.repository.AuthorDAO;
 import com.example.bookapimanytomany.repository.BookDAO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -22,6 +23,13 @@ public class AuthorController {
         this.bookDAO = bookDAO;
     }
 
+    @Operation(
+            summary = "gets all authors",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Responds with a list of Author entities."),
+                    @ApiResponse(responseCode = "400", description = "Author by the given id not found.")
+            }
+    )
     @GetMapping("/authors")
     public List<Author> findAllAuthors() {
         return authorDAO.findAll();
